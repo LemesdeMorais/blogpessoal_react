70342f28323e1d00cf86227d9698a85a20146f23
Rafaela Lemes <r.lemesdemorais@gmail.com>
2025-11-11 11:50:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from "./pages/home/Home"
+
+
+function App() {
+  
+
+  return (
+    <> {/*poderia ser uma div também, o importante é estar dentro de uma caixa */}
+    
+    <Home />
+  </>
+  )
+}
+
+export default App
